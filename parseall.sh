dcca9a4aa1fabebf3a63a6f481834f7b924117cb
billmills <you@example.com>
2022-04-04 11:49:04 -0400

    rc

--- parseall.sh
+++ parseall.sh
@@ -1,6 +1,6 @@
-python tcload.py 'HURDAT_ATLANTIC.csv' > atlantic.log
-python tcload.py 'HURDAT_PACIFIC.csv' > pacific.log
-python tcload.py 'JTWC_INDIANOCEAN.csv' > indian.log
-python tcload.py 'JTWC_SOUTHERNHEMISPHERE.csv' > southern.log
-python tcload.py 'JTWC_WESTPACIFIC.csv' > westpac.log
+python tcload.py 'data/HURDAT_ATLANTIC.csv' > atlantic.log
+python tcload.py 'data/HURDAT_PACIFIC.csv' > pacific.log
+python tcload.py 'data/JTWC_INDIANOCEAN.csv' > indian.log
+python tcload.py 'data/JTWC_SOUTHERNHEMISPHERE.csv' > southern.log
+python tcload.py 'data/JTWC_WESTPACIFIC.csv' > westpac.log
 sleep 10000000 # keep the container alive so we can fetch the logfiles; write to a volume instead once more storage becomes available
