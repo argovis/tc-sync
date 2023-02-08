18f21ad1227efa0d003a8c4b58ebc237f7acc198
billmills <you@example.com>
2023-02-08 18:40:55 -0500

    typo

--- summary-computation.py
+++ summary-computation.py
@@ -8,17 +8,17 @@ db = client.argo
 # metadata IDs <-> cyclone selection list
 agg = [
     {'$sort': {'timestamp':1}},
-    {'$lookup':{'from':'tcMetax','localField':'metadata','foreignField':'_id','as':'demo'}},
+    {'$lookup':{'from':'tcMeta','localField':'metadata','foreignField':'_id','as':'demo'}},
     {'$group': {'_id':'$metadata', 'date':{'$first':{'$year':'$timestamp'}}, 'name':{'$first':{'$first':'$demo.name'}}}},
     {'$project': {'label': {'$concat': ['$name', ' - ', {'$toString':'$date'}] }}},
     {'$sort': {'label':1}}
 ]
 
-tcs = list(db.tcx.aggregate(agg))
+tcs = list(db.tc.aggregate(agg))
 tcs = [{'_id': doc['_id'][0], 'label': doc['label']} for doc in tcs]
 
 try:
-    db.summariesx.replace_one({"_id": 'tc_labels'}, {"_id": 'tc_labels', "summary":tcs}, upsert=True)
+    db.summaries.replace_one({"_id": 'tc_labels'}, {"_id": 'tc_labels', "summary":tcs}, upsert=True)
 except BaseException as err:
     print('error: db write failure')
     print(err)
